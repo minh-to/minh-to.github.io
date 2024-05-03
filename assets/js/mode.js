4390900826f43a0e7f201b286f5082597f1a3e4f
mtt <mtt@duck.com>
2024-05-03 13:10:16 +0200

    fix flaticon url, fix faulty mode switching

--- assets/js/mode.js
+++ assets/js/mode.js
@@ -1,19 +1,21 @@
 const toggleSwitch = document.querySelector("#dark-mode-toggle");
-localStorage.setItem("theme", "dark"); // dark mode as default
 
 if (currentTheme) {
-  document.documentElement.setAttribute("data-theme", currentTheme);
+    document.documentElement.setAttribute("data-theme", currentTheme);
+} else {
+    localStorage.setItem("theme", "dark"); // dark mode as default
+    document.documentElement.setAttribute("data-theme", "dark");
 }
 
 function switchTheme() {
-  const currentTheme = document.documentElement.getAttribute("data-theme");
-  if (currentTheme === "dark") {
-    document.documentElement.setAttribute("data-theme", "light");
-    localStorage.setItem("theme", "light");
-  } else {
-    document.documentElement.setAttribute("data-theme", "dark");
-    localStorage.setItem("theme", "dark");
-  }
+    const currentTheme = document.documentElement.getAttribute("data-theme");
+    if (currentTheme === "dark") {
+        document.documentElement.setAttribute("data-theme", "light");
+        localStorage.setItem("theme", "light");
+    } else {
+        document.documentElement.setAttribute("data-theme", "dark");
+        localStorage.setItem("theme", "dark");
+    }
 }
 
 toggleSwitch.addEventListener("click", switchTheme, false);
