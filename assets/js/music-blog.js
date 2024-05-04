50f16d8cc006dec8b02e38b48239eed7dbbc5af2
mtt <mtt@duck.com>
2024-05-04 23:50:28 +0200

    add close button for modals

--- assets/js/music-blog.js
+++ assets/js/music-blog.js
@@ -1,15 +1,17 @@
 ---
 ---
-const regex= /height="[\d]{1,}"/gi;
+
+const regex = /height="[\d]{1,}"/gi;
+
 window.onload = function () {
     document.querySelectorAll(".article").forEach((article) => {
         article.addEventListener("click", function (e) {
             // Get iframe, and resize it for the pop-up
             let embed = article.getElementsByClassName("embed-container")[0].innerHTML;
             let maxHeight = document.documentElement.clientHeight;
-            embed = embed.replaceAll(regex, 'height=\"'+ maxHeight * 0.65 +'\"')
+            embed = embed.replaceAll(regex, 'height=\"' + maxHeight * 0.65 + '\"')
             let html = ``
-                html += `
+            html += `
           <h4>${article.title}</h4>
         <div>
        ${embed}
@@ -22,10 +24,13 @@ window.onload = function () {
         });
     });
 
-    document.querySelector("#category-modal-bg").addEventListener("click", function(){
-        document.querySelector("#category-modal-title").innerText = "";
-        document.querySelector("#category-modal-content").innerHTML = "";
-        document.querySelector("#category-modal-bg").classList.toggle("open");
-        document.querySelector("#category-modal").classList.toggle("open");
-    })
+    document.querySelector("#category-modal-bg").addEventListener("click", () => closeModal());
+    document.querySelector("#modal-close-button").addEventListener("click", () => closeModal());
 };
+
+function closeModal() {
+    document.querySelector("#category-modal-title").innerText = "";
+    document.querySelector("#category-modal-content").innerHTML = "";
+    document.querySelector("#category-modal-bg").classList.toggle("open");
+    document.querySelector("#category-modal").classList.toggle("open");
+}
